3e0e923c9b81f05a99131085ad40ae03b5ef529e
yuhao.ju <yuhao.ju@qunar.com>
2018-01-10 11:41:25 +0800

    feat: add transform-function-bind

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ exports.config = function (options, cwd) {
             "transform-class-properties",
             "transform-decorators-legacy",
             "transform-object-rest-spread",
-            "transform-object-assign"
+            "transform-object-assign",
+            "transform-function-bind"
         ]
     }
 
